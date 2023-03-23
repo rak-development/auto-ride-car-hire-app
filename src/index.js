ebe9b43fff153f3c35aa660459b9c10704f2f039
Rafal <rafal.krohne@oneadvanced.com>
2023-03-23 15:55:36 +0100

    Fixing server

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './style.css'
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!')
+} else {
+  console.log('Looks like we are in production mode!')
 }
 
 function component () {
