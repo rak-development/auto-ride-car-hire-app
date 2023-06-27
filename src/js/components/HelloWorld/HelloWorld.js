6273331a3be48577e3b932e1a2aa30b5452c3136
Dawid Bulinski <dawid.bulinski@beamery.com>
2023-06-27 13:23:10 +0200

    feat(hello-world): fixed custom element

--- src/js/components/HelloWorld/HelloWorld.js
+++ src/js/components/HelloWorld/HelloWorld.js
@@ -3,20 +3,26 @@ class HelloWorld extends HTMLElement {
 
   constructor () {
     super()
-    this.attachShadow({ mode: 'open' })
+    this.attachShadow({ mode: 'open' });
+    this.posts = [{ id: '1', content: 'Hello' }, { id: '2', content: 'World' }];
   }
 
   set posts (value) {
-    this.render()
+    this.#posts = value;
+    this.render();
   }
 
   render () {
+    const posts = this.#posts.map(post => `<li data-id="${post.id}">${post.content}</li>`).join('');
     this.shadowRoot.innerHTML = `
       <section class="blog-posts">
         <slot name="heading"><h2>Blog Posts</h2></slot>
         <div class="posts">
           POSTS
-          </div>
+        </div>
+        <ul>
+        ${posts}
+        </ul>
       </section>
     `
   }
