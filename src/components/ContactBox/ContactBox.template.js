9a2567be854567762af3b96d4fd9f037823e6c4a
Rafal <rafal.krohne@oneadvanced.com>
2023-07-24 11:23:58 +0200

    Adding dynamic data use for contact box

--- src/components/ContactBox/ContactBox.template.js
+++ src/components/ContactBox/ContactBox.template.js
@@ -1,44 +1,20 @@
-const template = styles => {
+const template = (styles, contactData) => {
   return `
     <div class='row'>
-      <div class='col-md-4'>
-        <div class='${styles['contact-box']} d-flex'>
-          <div class='${styles['contact-box__icon-container']}'>
-            <i class="fa-regular fa-clock ${styles['contact-box__icon']}"></i>
-            <span></span>
-          </div>
-          <div class='${styles['contact-box__content']}'>
-            <div class='${styles['contact-box__title']}'>ADDRESS</div>
-            <div class='${styles['contact-box__text']}'>Jasionka 942, <br />36-002 Jasionka</div>
-          </div>
-        </div>
-      </div>
-      <div class='col-md-4'>
-        <div class='${styles['contact-box']} d-flex'>
-          <div class='${styles['contact-box__icon-container']}'>
-            <i class="fa-regular fa-clock ${styles['contact-box__icon']}"></i>
-            <span></span>
-          </div>
-          <div class='${styles['contact-box__content']}'>
-            <div class='${styles['contact-box__title']}'>PHONES</div>
-            <div class='${styles['contact-box__text']}'>
-              BOOK A RIDE: 123 456 789 <br />OFFICE: (+48) 17 123 45 61
+      ${contactData.map(({title, icon, text}) => `
+        <div class='col-md-4'>
+          <div class='${styles['contact-box']} d-flex'>
+            <div class='${styles['contact-box__icon-container']}'>
+              <i class='${icon} ${styles['contact-box__icon']}'></i>
+              <span></span>
+            </div>
+            <div class='${styles['contact-box__content']}'>
+              <div class='${styles['contact-box__title']}'>${title}</div>
+              <div class='${styles['contact-box__text']}'>${text}</div>
             </div>
           </div>
         </div>
-      </div>
-      <div class='col-md-4'>
-        <div class='${styles['contact-box']} d-flex'>
-          <div class='${styles['contact-box__icon-container']}'>
-            <i class="fa-regular fa-clock ${styles['contact-box__icon']}"></i>
-            <span></span>
-          </div>
-          <div class='${styles['contact-box__content']}'>
-            <div class='${styles['contact-box__title']}'>WORKING HOURS</div>
-            <div class='${styles['contact-box__text']}'>24/7</div>
-          </div>
-        </div>
-      </div>
+      `).join('')}
     </div>
   `
 }
