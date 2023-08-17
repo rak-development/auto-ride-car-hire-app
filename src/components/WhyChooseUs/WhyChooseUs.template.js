633914f255571f36c51502e4791394bfc9af16d6
Rafal <rafal.krohne@oneadvanced.com>
2023-08-17 19:57:58 +0200

    Adding some content and styling

--- src/components/WhyChooseUs/WhyChooseUs.template.js
+++ src/components/WhyChooseUs/WhyChooseUs.template.js
@@ -5,6 +5,23 @@ const template = (styles, sectionData) => {
         <div class='${styles['section-template__subheader']}'>${sectionData.subheader}</div>
         <div class='${styles['section-template__header']}'>${sectionData.header}</div>
       </div>
+      <div class='container'>
+        <div class='${styles['core-values__container']} position-relative'>
+          <div class='${styles['core-values__circle']}'>
+            <span class='d-block'>7 CORE VALUES</span>
+            <span class='d-block'>That make us the best transportation company in the area</span>
+          </div>
+          <div class='${styles['core-values__circle']}'>
+            <div class='${styles['core-values__circle-content']}'>
+              <span class="theme-component-feature-circle-item-circle">
+                <span class="theme-icon-meta-tick-3"></span>
+              </span>
+              <span class="theme-component-feature-circle-item-label">FIRST-RATE CUSTOMER SERVICE</span>
+            </div>
+          </div>
+          <div class='${styles['core-values__circle']}'></div>
+        </div>
+      </div>
     </div>
   `
 }
