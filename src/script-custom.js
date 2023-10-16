7c98ef06d2dd944bfb9bbcaa60d1e3b3e8f55d30
Rafal <rafal.krohne@oneadvanced.com>
2023-10-16 10:57:12 +0200

    Code review changes - simplifying/tidying up the code

--- src/script-custom.js
+++ src/script-custom.js
@@ -1,14 +1,12 @@
 const scrollToTopButton = document.getElementById('scroll-to-top')
 
-const scrollFunction = () => {
-  if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 3000) {
-    scrollToTopButton.style.opacity = '1'
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 3000) {
+    scrollToTopButton.classList.add('d-flex')
   } else {
-    scrollToTopButton.style.opacity = '0'
+    scrollToTopButton.classList.remove('d-flex')
   }
-}
-
-window.onscroll = () => { scrollFunction() }
+});
 
 scrollToTopButton.addEventListener('click', () => {
   document.body.scrollTop = 0 // For Safari
