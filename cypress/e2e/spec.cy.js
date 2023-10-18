ead598a257331a20a9c97bb494fe3035a34ccf06
Rafal <rafal.krohne@oneadvanced.com>
2023-10-18 14:07:56 +0200

    Adding test for validation

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -57,4 +57,44 @@ describe('The Home Page', () => {
     })
   })
 
+  it('should throw validation error during submitting the form', () => {
+    const pickupLocation = 'Warsaw'
+    const dateFrom = '2023-12-01'
+    const pickupTime = '10:00'
+    const dateTo = '2023-12-10'
+    const dropOffTime = '11:00'
+    const isOver25 = 'Yes'
+    const discountCode = 'A1B2B3'
+
+    cy.get('#submittedDetailsModal').should('not.be.visible')
+
+    cy.findByLabelText('Pickup Location').should('be.visible').type(pickupLocation)
+    cy.findByLabelText('Date From').type(dateFrom)
+    cy.findByLabelText('Pick-up Time').select(pickupTime)
+    cy.findByLabelText('Date To').type(dateTo)
+    cy.findByLabelText('Drop-off Time').select(dropOffTime)
+    cy.findByLabelText('Is driver over 25 years old?').check()
+
+    cy.findByPlaceholderText('Discount Code').should('not.be.visible')
+    cy.findByLabelText('I have discount code').check()
+    cy.findByPlaceholderText('Discount Code').should('be.visible').type(discountCode)
+
+    cy.get('#bookingReservation')
+      .findByRole('button', {name: /Find Cars/i})
+      .should('exist')
+
+    cy.get('#bookingReservation').submit()
+
+    cy.get('#submittedDetailsModal').should('be.visible')
+    cy.get('#submittedDetailsModal').within(() => {
+      cy.contains(pickupLocation)
+      cy.contains(dateFrom)
+      cy.contains(pickupTime)
+      cy.contains(dateTo)
+      cy.contains(dropOffTime)
+      cy.contains(isOver25)
+      cy.contains(discountCode)
+    })
+  })
+
 })
