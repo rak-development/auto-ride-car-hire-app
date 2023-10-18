c41065663b8ef7b749270754a1c5472ccc5489f5
Rafal <rafal.krohne@oneadvanced.com>
2023-10-18 12:58:13 +0200

    Code review changes - simplifying/tidying up the code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,31 +1,55 @@
 describe('The Home Page', () => {
-
-  it('successfully loads', () => {
+  beforeEach(() => {
     cy.visit('/')
   })
 
-  it('test scroll to top', () => {
-    cy.visit('/')
-    cy.get('#scroll-to-top').should('exist', 'display', 'none')
-    cy.scrollTo(0, 3001)
-    cy.get('#scroll-to-top').should('exist', 'display', 'flex')
+  it('should show scroll-to-top button and scroll the page to top on click', () => {
+    const windowTopPosition = 0
+    const minimumScrollPositionToDisplayScrollToTopButton = 3001
+
+    cy.get('#scroll-to-top').should('not.be.visible')
+    cy.scrollTo(windowTopPosition, minimumScrollPositionToDisplayScrollToTopButton)
+    cy.get('#scroll-to-top').should('be.visible')
     cy.get('#scroll-to-top').click()
-    cy.window().its('scrollY').should('equal', 0);
+    cy.window().its('scrollY').should('equal', 0)
   })
 
-  it('find car form submit', () => {
-    cy.visit('/')
-    cy.get('#bookingReservation').should('exist').then(() => {
-      cy.get('#pickupLocation').should('exist').type("Warsaw")
-      cy.get('#dropOffLocation').should('exist').type("Warsaw")
-      cy.get('#pickupDate').should('exist').type("2023-12-01")
-      cy.get('#pickupTime').should('exist').type("9:00")
-      cy.get('#dropOffDate').should('exist').type("2023-12-10")
-      cy.get('#pickupTime').should('exist').type("9:00")
-      cy.get('#over25').should('exist').check({ force: true })
+  it('should find the Pickup Location label and fill the input ', () => {
+    cy.findByLabelText('Pickup Location').type('Warsaw')
+  })
 
-      cy.findByRole('button', {name: /Find Cars/i}).click()
-    })
+  it('should find the Drop-off Location label and fill the input ', () => {
+    cy.findByLabelText('Drop-off Location').type('Warsaw')
+  })
+
+  it('should find the Date From label and fill the input ', () => {
+    cy.findByLabelText('Date From').type('2023-12-01')
+  })
+
+  it('should find the Pick-up Time label and select value ', () => {
+    cy.findByLabelText('Pick-up Time').select('10:00')
+  })
+
+  it('should find the Date To label and fill the input ', () => {
+    cy.findByLabelText('Date To').type('2023-12-10')
+  })
+
+  it('should find the Drop-off Time label and select value ', () => {
+    cy.findByLabelText('Drop-off Time').select('10:00')
+  })
+
+  it('should find the Is driver over 25 years old? and check the input ', () => {
+    cy.findByLabelText('Is driver over 25 years old?').check()
+  })
+
+  it('should find the I have discount code and check the input ', () => {
+    cy.findByLabelText('I have discount code').check()
+  })
+
+  it('should find the Discount Code input and fill the input ', () => {
+    cy.findByPlaceholderText('Discount Code').should('not.be.visible')
+    cy.findByLabelText('I have discount code').check()
+    cy.findByPlaceholderText('Discount Code').should('be.visible').type('A1B2B3')
   })
 
 })
