230d4ace3e670a5b3fbdd34079614f17b7668694
Rafal <rafal.krohne@oneadvanced.com>
2023-10-19 12:34:01 +0200

    Code review changes - removing unused code and tidying up

--- cypress.config.js
+++ cypress.config.js
@@ -3,20 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: 'pdm3nr',
   e2e: {
-    baseUrl: 'http://localhost:8000',
-    setupNodeEvents(on) {
-      on('task', {
-        log(message) {
-          console.log(message)
-    
-          return null
-        },
-        table(message) {
-          console.table(message)
-    
-          return null
-        }
-      })
-    },
+    baseUrl: 'http://localhost:8000'
   },
 });
--- cypress/e2e/accessibility.cy.js
+++ cypress/e2e/accessibility.cy.js
@@ -1,10 +1,10 @@
 describe('The Home Page Accessibility', () => {
   beforeEach(() => {
     cy.visit('/')
+    cy.injectAxe()
   })
 
   it('Has no detectable a11y violations on load', () => {
-    cy.injectAxe()
     cy.checkA11y()
   })
 
