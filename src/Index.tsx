f0b3ef697555203db1039b866fe1c786c6071da7
Rafal <rafal.krohne@oneadvanced.com>
2023-10-20 12:30:22 +0200

    Tidying up the code and file naming

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,16 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import App from './App';
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('root'),
-// );
-
 import { createRoot } from "react-dom/client";
-import App from './App';
+import App from './app';
 
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById("app") as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
