aaa96e5622a351d12ab9072c6c4a9d81d07118f5
Rafal <rafal.krohne@oneadvanced.com>
2023-10-25 07:14:30 +0200

    Creating contact box component

--- src/components/Footer/footer-top/footer-top.component.tsx
+++ src/components/Footer/footer-top/footer-top.component.tsx
@@ -1,12 +1,13 @@
 import Container from 'react-bootstrap/Container';
 
+import { ContactBox } from '../../contact-box/contact-box.component';
 import { FooterTopWrapper } from './footer-top-styles'
 
 export const FooterTop: React.FC = () => {
   return (
     <FooterTopWrapper>
       <Container>
-      Footer Top
+        <ContactBox />
       </Container>
     </FooterTopWrapper>
   )
---
+++ src/components/contact-box/contact-box.component.tsx
@@ -0,0 +1,7 @@
+export const ContactBox: React.FC = () => {
+  return (
+    <div>
+      Contact Box
+    </div>
+  )
+}
