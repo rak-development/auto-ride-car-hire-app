f6c3e126bd038aa7104ae909e58eecc5c47d77e2
Rafal <rafal.krohne@oneadvanced.com>
2023-11-01 21:21:56 +0100

    Working on migrating scroll to top into React/TS

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,38 @@
-import { Header } from "./components/header/header.component";
+import { Header } from './components/header/header.component';
+import { ScrollToTop } from './components/scroll-to-top/scroll-to-top.component';
 
 import './app.scss';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {   
+    window.addEventListener('scroll', listenToScroll);
+    return () => 
+       window.removeEventListener('scroll', listenToScroll); 
+  }, [])
+  
+  const listenToScroll = () => {
+    let heightToHideFrom = 3000;
+    const winScroll = document.body.scrollTop || 
+        document.documentElement.scrollTop;
+
+    if (winScroll > heightToHideFrom) {  
+      !isVisible && setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }  
+  };
+  
   return (
     <>
       <Header />
-      <main>Main</main>
+      <main>
+        <h1 style={{ marginTop: 2000, marginBottom: 2000}}>Welcome to my app</h1>
+        {isVisible && <ScrollToTop />}
+      </main>
       <footer>Footer</footer>
-      <h1>Welcome to my app</h1>
     </>
   );
 }
