321dbabacce871087fb592928f56153d638ccdf2
Rafal <rafal.krohne@oneadvanced.com>
2023-11-01 21:23:12 +0100

    Removing unused files

--- src/components/scroll-to-top/ScrollToTop.js
+++
@@ -1,8 +0,0 @@
-import template from './ScrollToTop.template'
-import styles from './ScrollToTop.module.scss'
-
-export class ScrollToTop extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = template(styles)
-  }
-}
