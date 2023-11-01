c13a34b87a2aac19ea0a0ea90cd66da9a39cdfee
Rafal <rafal.krohne@oneadvanced.com>
2023-11-01 20:38:15 +0100

    Working on migrating our fleet and section template into React/TS

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
 import { Header } from "./components/header/header.component";
 
 import './app.scss';
+import { OurFleet } from "./components/our-fleet/our-fleet.component";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main>Main</main>
+      <main>
+        <OurFleet />
+      </main>
       <footer>Footer</footer>
       <h1>Welcome to my app</h1>
     </>
---
+++ src/types/our-fleet-data-type.ts
@@ -0,0 +1,7 @@
+export type OurFleetDataType = {
+  id: number,
+  image: string,
+  title: string,
+  passengerNumber: number,
+  luggageNumber: number
+}
