35aabe742e4890a087940af8ff379de57752294f
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2023-11-15 22:35:50 +0100

    Moving testimonials data into json, removing old unused files, finishing off functionality and styling of the carousel

--- src/components/testimonials/testimonials.component.tsx
+++ src/components/testimonials/testimonials.component.tsx
@@ -5,9 +5,15 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Carousel from 'react-bootstrap/Carousel';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+
 import styled from "@emotion/styled"
 
 import imageUrl from '/src/assets/images/AutoRide-Testimonials.jpeg'
+
+import TESTIMONIALS_DATA from '../../testimonials-data.json'
+import { device } from '../../devices-breakpoints';
 
 
 const TestimonialsWrapper = styled.div`
@@ -18,8 +24,12 @@ const TestimonialsSliderCol = styled(Col)`
   padding: 0;
 `
 
-const TestimonialsCarousel = styled.div`
+const TestimonialsCarouselWrapper = styled.div`
   padding: 3.125rem 1rem;
+
+  @media ${device.lg} {
+    padding: 6.25rem 5rem;
+  }
 `
 
 const TestimonialsImageCol = styled(Col)`
@@ -39,6 +49,89 @@ const TestimonialsImage = styled.div<ImageURLprops>`
   flex-grow: 1;
 `
 
+const TheCarousel = styled(Carousel)`
+  .carousel-indicators {
+    margin-bottom: 0;
+  }
+
+  .carousel-indicators [data-bs-target] {
+    width: 0.5rem;
+    height: 0.5rem;
+    background-color: var(--bs-gray-500);
+    border: none;
+    border-radius: 50%;
+    outline: none;
+    transition: background-color ease 250ms;
+    cursor: pointer;
+    border-width: 0.125rem;
+    border-style: solid;
+    border-radius: 0.6875rem;
+    border-color: var(--bs-gray-500);
+    display: block;
+  }
+
+  .carousel-indicators .active {
+    background-color: transparent;
+    pointer-events: none;
+    border-color: var(--bs-primary);
+  }
+
+  .carousel-caption {
+    position: relative;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    padding: 0;
+  }
+`
+
+const TestimonialIconWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  margin-top: 10px;
+`
+
+const TestimonialIcon = styled(FontAwesomeIcon)`
+  color: var(--bs-primary);
+  width: 3rem;
+  height: 3rem;
+  z-index: 2;
+  top: -0.625rem;
+  left: -0.625rem;
+  position: absolute;
+`
+
+const TestimonialIconBackground = styled.span`
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  background-color: var(--bs-white);
+  display: block;
+`
+
+const TestimonialQuote = styled.div`
+  font-size: 1.25rem;
+  font-weight: 300;
+  line-height: 1.63636em;
+  padding: 0.625rem 0;
+  color: var(--bs-black);
+
+  @media ${device.lg} {
+    font-size: 1.375rem;
+  }
+`
+
+const TestimonialAuthor = styled.div`
+  font-size: 0.875rem;
+  font-weight: 400;
+  text-transform: uppercase;
+  line-height: 1.125rem;
+  letter-spacing: 0.125rem;
+  margin-top: 1.875rem;
+  color: var(--bs-black);
+  padding-bottom: 13.125rem;
+`
+
 export const Testimonials = () => {
   const [index, setIndex] = useState(0);
 
@@ -51,33 +144,22 @@ export const Testimonials = () => {
       <Container fluid>
         <Row>
           <TestimonialsSliderCol md={6}>
-            <TestimonialsCarousel>
-              <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                  {/* <ExampleCarouselImage text="First slide" /> */}
-                  <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  {/* <ExampleCarouselImage text="Second slide" /> */}
-                  <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  {/* <ExampleCarouselImage text="Third slide" /> */}
-                  <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                      Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              </Carousel>
-            </TestimonialsCarousel>
+            <TestimonialsCarouselWrapper>
+              <TheCarousel activeIndex={index} onSelect={handleSelect} controls={false}>
+                {TESTIMONIALS_DATA.map(({id,text,author}) => (
+                  <Carousel.Item key={id}>
+                    <Carousel.Caption>
+                      <TestimonialIconWrapper>
+                        <TestimonialIcon icon={faComments} />
+                        <TestimonialIconBackground />
+                      </TestimonialIconWrapper>
+                      <TestimonialQuote>{text}</TestimonialQuote>
+                      <TestimonialAuthor>{author}</TestimonialAuthor>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                ))}
+              </TheCarousel>
+            </TestimonialsCarouselWrapper>
           </TestimonialsSliderCol>
           <TestimonialsImageCol md={6}>
             <TestimonialsImage imageUrl={imageUrl} />
