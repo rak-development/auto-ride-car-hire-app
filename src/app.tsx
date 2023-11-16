77a78f499fc9b32ba36a81663524d757ab74e200
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2023-11-16 10:02:36 +0100

    Preparing why choose us component, renaming folder, setting up basic component

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { Header } from './components/header/header.component'
 import { Footer } from './components/footer/footer.component'
 import { Introduction } from './components/introduction/introduction.component'
 import { WhatWeOffer } from './components/what-we-offer/what-we-offer.component'
+import { WhyChooseUs } from './components/why-choose-us/why-choose-us.component'
 import { KeyFeatures } from './components/key-features/key-features.component'
 
 import './app.scss'
@@ -12,6 +13,7 @@ const App = () => (
     <main>
       <Introduction />
       <WhatWeOffer />
+      <WhyChooseUs />
       <KeyFeatures />
     </main>
     <Footer />
---
+++ src/components/why-choose-us/why-choose-us.component.tsx
@@ -0,0 +1,13 @@
+import { SectionTemplate } from "../section-template/section-template.component"
+
+export const WhyChooseUs = () => {
+  return (
+    <SectionTemplate
+      subheader='Why Choose Us'
+      header='Proudly Serving the Oakland Area Since 2007'
+      bgMode='--bs-white'
+    >
+      <div>Why Choose Us</div>
+    </SectionTemplate>
+  )
+}
