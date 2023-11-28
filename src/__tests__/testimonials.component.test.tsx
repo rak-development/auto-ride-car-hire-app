4247951bfe1232c3352f5f2422f542f544e30543
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2023-11-28 20:15:52 +0100

    Adding first test file

---
+++ src/__tests__/testimonials.component.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+
+import {Testimonials} from "../components/testimonials/testimonials.component";
+
+describe("<Testimonials />", () => {
+  test("should display", async () => {
+    // ???
+  });
+});
