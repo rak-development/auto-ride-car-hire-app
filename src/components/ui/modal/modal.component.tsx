2e3a9b35676ad72ee6f31bb4dc22d3bf0bde6852
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2024-02-14 15:25:06 +0100

    Code review changes - simplifying the code

--- src/components/ui/modal/modal.component.tsx
+++ src/components/ui/modal/modal.component.tsx
@@ -5,21 +5,6 @@ import { format } from "date-fns";
 import { type FormData } from '../../booking-reservation/booking-reservation.component';
 import ModalElement from './modal-element/modal-element.component';
 
-const camelCaseToWords = (string: string) => {
-  const result = string.replace(/([a-z0-9])([A-Z])/g, '$1 $2');
-  return result.charAt(0).toUpperCase() + result.slice(1);
-}
-
-const userFriendlyText = (value: string | boolean | Date) => {
-  if ('boolean' === typeof value) {
-    return value ? 'Yes' : 'No'
-  } else if (value instanceof Date) {
-    return format(value, 'PPPppp');
-  } else {
-    return value
-  }
-}
-
 interface ModalComponentProps {
   formData: FormData;
   showModal: boolean | undefined;
@@ -33,12 +18,13 @@ const ModalComponent = ({formData, showModal, onClose}: ModalComponentProps) =>
         <Modal.Title>Your submitted details</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        {Object.entries(formData).map(([key, value]) => {
-          const questionTitle = camelCaseToWords(key);
-          const questionAnswer = userFriendlyText(value);
-
-          return <ModalElement key={key} title={questionTitle} answer={questionAnswer} />} 
-        )}
+        <ModalElement title="Pickup Location" answer={formData.pickupLocation} />
+        <ModalElement title="Date From" answer={format(formData.pickupDate, 'PPPppp')} />
+        <ModalElement title="Drop-off Location" answer={formData.dropOffLocation} />
+        <ModalElement title="Date To" answer={format(formData.dropOffDate, 'PPPppp')} />
+        <ModalElement title="Is driver over 25 years old?" answer={formData.isOver25 ? 'Yes' : 'No'} />
+        <ModalElement title="I have discount code" answer={formData.dropOffLocation ? 'Yes' : 'No'} />
+        {formData.dropOffLocation && formData.discountCode && <ModalElement title="Discount code" answer={formData.discountCode} />}
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onClose}>
