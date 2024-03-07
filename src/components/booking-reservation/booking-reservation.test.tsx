ead6f088692f992f0fb035efc57b7bdff62821d6
Rafal Krohne <rafal.krohne@hotmail.co.uk>
2024-03-07 16:23:22 +0100

    Code Review Changes - code improvement and fixing formatting

--- src/components/booking-reservation/booking-reservation.test.tsx
+++ src/components/booking-reservation/booking-reservation.test.tsx
@@ -2,20 +2,17 @@ import userEvent from '@testing-library/user-event'
 import { addDays, subDays, format } from 'date-fns'
 
 import { render, screen } from '../../utils/test-utils'
-import { BookingReservation } from "./booking-reservation.component";
+import { BookingReservation } from './booking-reservation.component'
 
-const dateFormatForForm = (dateObject: Date) => format(new Date(dateObject), "yyyy-MM-dd'T'HH:mm:ss")
+const dateFormatForForm = (dateObject: Date) => format(dateObject, "yyyy-MM-dd'T'HH:mm:ss")
 
 describe('Booking Reservation', () => {
-
   beforeEach(() => {
-    render(
-        <BookingReservation />
-    );
+    render(<BookingReservation />)
   })
 
   it('should test validation on empty fields in the reservation form', async () => {
-    const submitButton = screen.getByRole('button', { name: 'Find Cars'})
+    const submitButton = screen.getByRole('button', { name: 'Find Cars' })
     const user = userEvent.setup()
     await user.click(submitButton)
 
@@ -34,56 +31,59 @@ describe('Booking Reservation', () => {
       dropOffDate: addDays(new Date(), 10),
     }
 
-    await userEvent.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation);
-    await userEvent.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation);
-    await userEvent.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate));
-    await userEvent.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate));
+    await userEvent.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation)
+    await userEvent.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation)
+    await userEvent.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate))
+    await userEvent.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate))
     await userEvent.click(screen.getByLabelText('Is driver over 25 years old?'))
     await userEvent.click(screen.getByLabelText('I have discount code'))
 
     expect(await screen.findByText('Please provide a discount code.')).toBeInTheDocument()
-
   })
 
   it('should test filling in the form', async () => {
-    expect(screen.queryByTestId("modal")).not.toBeInTheDocument()
+    expect(screen.queryByRole('modal')).not.toBeInTheDocument()
 
     const formData = {
       pickupLocation: 'Rzeszów Główny PKP',
       dropOffLocation: 'Millenium Hall Rzeszów',
       pickupDate: addDays(new Date(), 5),
       dropOffDate: addDays(new Date(), 10),
-      discountCode: 'HIT15'
+      discountCode: 'HIT15',
     }
 
     const user = userEvent.setup()
 
-    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation);
-    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation);
-    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate));
-    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate));
+    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation)
+    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation)
+    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate))
+    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate))
     await user.click(screen.getByLabelText('Is driver over 25 years old?'))
     await user.click(screen.getByLabelText('I have discount code'))
-    await user.type(screen.getByLabelText('Discount Code'), formData.discountCode);
+    await user.type(screen.getByLabelText('Discount Code'), formData.discountCode)
 
-    const submitButton = screen.getByRole('button', { name: 'Find Cars'})
+    const submitButton = screen.getByRole('button', { name: 'Find Cars' })
     await user.click(submitButton)
 
-    const modal = screen.getByRole('dialog', { hidden: true })
-    expect(modal).toBeInTheDocument();
+    const modal = screen.getByRole('dialog')
+    expect(modal).toBeInTheDocument()
 
     expect(screen.getByText('Pickup Location : Rzeszów Główny PKP')).toBeInTheDocument()
     expect(screen.getByText('Drop-off Location : Millenium Hall Rzeszów')).toBeInTheDocument()
-    expect(screen.getByText(`Date From : ${format(formData.pickupDate, 'PPPppp')}`)).toBeInTheDocument()
-    expect(screen.getByText(`Date To : ${format(formData.dropOffDate,  'PPPppp')}`)).toBeInTheDocument()
+    expect(
+      screen.getByText(`Date From : ${format(formData.pickupDate, 'PPPppp')}`),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(`Date To : ${format(formData.dropOffDate, 'PPPppp')}`),
+    ).toBeInTheDocument()
 
     expect(screen.getByText('Is driver over 25 years old? : Yes')).toBeInTheDocument()
     expect(screen.getByText('I have discount code : Yes')).toBeInTheDocument()
     expect(screen.getByText(`Discount code : ${formData.discountCode}`)).toBeInTheDocument()
 
-    const closeButton = screen.getAllByRole('button', { name: 'Close'})[1]
+    const closeButton = screen.getAllByRole('button', { name: 'Close' })[1]
     await user.click(closeButton)
-    expect(screen.queryByRole('dialog', { hidden: true })).not.toBeInTheDocument();
+    expect(screen.queryByRole('dialog', { hidden: true })).not.toBeInTheDocument()
   })
 
   it('should test pick-up date validation if date is in the past', async () => {
@@ -91,17 +91,17 @@ describe('Booking Reservation', () => {
       pickupLocation: 'Rzeszów Główny PKP',
       dropOffLocation: 'Millenium Hall Rzeszów',
       pickupDate: subDays(new Date(), 2),
-      dropOffDate: addDays(new Date(), 2)
+      dropOffDate: addDays(new Date(), 2),
     }
 
     const user = userEvent.setup()
 
-    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation);
-    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation);
-    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate));
-    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate));
+    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation)
+    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation)
+    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate))
+    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate))
 
-    const submitButton = screen.getByRole('button', { name: 'Find Cars'})
+    const submitButton = screen.getByRole('button', { name: 'Find Cars' })
     await user.click(submitButton)
 
     expect(await screen.findByText('Pick-up date needs to be in the future')).toBeInTheDocument()
@@ -112,17 +112,17 @@ describe('Booking Reservation', () => {
       pickupLocation: 'Rzeszów Główny PKP',
       dropOffLocation: 'Millenium Hall Rzeszów',
       pickupDate: addDays(new Date(), 2),
-      dropOffDate: subDays(new Date(), 2)
+      dropOffDate: subDays(new Date(), 2),
     }
 
     const user = userEvent.setup()
 
-    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation);
-    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation);
-    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate));
-    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate));
+    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation)
+    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation)
+    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate))
+    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate))
 
-    const submitButton = screen.getByRole('button', { name: 'Find Cars'})
+    const submitButton = screen.getByRole('button', { name: 'Find Cars' })
     await user.click(submitButton)
 
     expect(await screen.findByText('Drop-off date needs to be in the future')).toBeInTheDocument()
@@ -133,19 +133,21 @@ describe('Booking Reservation', () => {
       pickupLocation: 'Rzeszów Główny PKP',
       dropOffLocation: 'Millenium Hall Rzeszów',
       pickupDate: addDays(new Date(), 10),
-      dropOffDate: addDays(new Date(), 8)
+      dropOffDate: addDays(new Date(), 8),
     }
 
     const user = userEvent.setup()
 
-    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation);
-    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation);
-    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate));
-    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate));
+    await user.type(screen.getByLabelText('Pickup Location'), formData.pickupLocation)
+    await user.type(screen.getByLabelText('Drop-off Location'), formData.dropOffLocation)
+    await user.type(screen.getByLabelText('Date From'), dateFormatForForm(formData.pickupDate))
+    await user.type(screen.getByLabelText('Date To'), dateFormatForForm(formData.dropOffDate))
 
-    const submitButton = screen.getByRole('button', { name: 'Find Cars'})
+    const submitButton = screen.getByRole('button', { name: 'Find Cars' })
     await user.click(submitButton)
 
-    expect(await screen.findByText('Drop-off date needs to be greater than pick-up date')).toBeInTheDocument()
+    expect(
+      await screen.findByText('Drop-off date needs to be greater than pick-up date'),
+    ).toBeInTheDocument()
   })
 })
