ca5a5eaaaae20a881c75b13302b734caf0ea5a86
Rafal Krohne <rafal.krohne@xebia.com>
2024-09-16 12:55:21 +0200

    Package-lock update and index to start from the beginning

--- src/index.tsx
+++ src/index.tsx
@@ -5,38 +5,43 @@ import App from './app'
 
 import './i18n'
 
-import './i18n'
+// let appReady = Promise.resolve();
 
-let appReady = Promise.resolve();
+// console.log('appReady: ', appReady)
 
-console.log('appReady: ', appReady)
+// // Enable API mocking only in development
+// if (process.env.NODE_ENV === "development") {
+//   const { worker } = require("./mocks/browser");
+//   appReady = worker.start({
+//     serviceWorker: {
+//       /**
+//        * Use a custom Service Worker script URL to resolve
+//        * the mock worker served by Codesandbox.
+//        * @note You DO NOT need this in your application.
+//        * @see https://mswjs.io/docs/api/setup-worker/start#serviceworker
+//        */
+//       url: "/mockServiceWorker.js"
+//     }
+//   });
+// }
 
-// Enable API mocking only in development
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  appReady = worker.start({
-    serviceWorker: {
-      /**
-       * Use a custom Service Worker script URL to resolve
-       * the mock worker served by Codesandbox.
-       * @note You DO NOT need this in your application.
-       * @see https://mswjs.io/docs/api/setup-worker/start#serviceworker
-       */
-      url: "/mockServiceWorker.js"
-    }
-  });
-}
+// /**
+//  * Use deferred application mounting in order to work in a sandbox.
+//  * You MAY NOT need this in your application.
+//  * @see https://mswjs.io/docs/recipes/deferred-mounting
+//  */
+// appReady.then(() => {
+//   const root = createRoot(document.getElementById('app') as HTMLElement)
+//   root.render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>
+//   );
+// });
 
-/**
- * Use deferred application mounting in order to work in a sandbox.
- * You MAY NOT need this in your application.
- * @see https://mswjs.io/docs/recipes/deferred-mounting
- */
-appReady.then(() => {
-  const root = createRoot(document.getElementById('app') as HTMLElement)
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-});
+const root = createRoot(document.getElementById('app') as HTMLElement)
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
