1981c788dff155c2138ef39978d55f36a777193c
Rafal Krohne <rafal.krohne@xebia.com>
2024-10-10 15:35:00 +0200

    Running format

--- src/components/introduction/introduction.test.tsx
+++ src/components/introduction/introduction.test.tsx
@@ -2,28 +2,27 @@ import { render } from '../../utils/test-utils'
 import { Introduction } from './introduction.component'
 
 import { http, HttpResponse } from 'msw'
-import { setupServer } from "msw/node";
+import { setupServer } from 'msw/node'
 
 const server = setupServer(
-    http.get("assets/db/introduction-data-en.json", () => {
-        return HttpResponse.json({greeting: 'hello there'})
-    })
-);
+  http.get('assets/db/introduction-data-en.json', () => {
+    return HttpResponse.json({ greeting: 'hello there' })
+  }),
+)
 
 describe('Introduction Data', () => {
-    beforeAll(() => server.listen())
-    afterEach(() => server.resetHandlers())
-    afterAll(() => server.close())
+  beforeAll(() => server.listen())
+  afterEach(() => server.resetHandlers())
+  afterAll(() => server.close())
 
-    test("displays data from backend", async () => {
-        server.use(
-            http.get('assets/db/introduction-data-en.json', () => {
-              return new HttpResponse(null, {status: 404})
-            }),
-          )
+  test('displays data from backend', async () => {
+    server.use(
+      http.get('assets/db/introduction-data-en.json', () => {
+        return new HttpResponse(null, { status: 404 })
+      }),
+    )
 
-        //   render(<Introduction />)
-      console.log('server: ', server.listen())
-    });
-
+    //   render(<Introduction />)
+    console.log('server: ', server.listen())
+  })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ import './i18n'
 
 async function deferRender() {
   const { worker } = await require('./mocks/worker.ts')
-  return worker.start({ onUnhandledRequest: (request: Request) => {
-    if (request.url.endsWith('.json')) {
-      console.warn(`Please add a handler for ${request.url}`);
-    }
-  } })
+  return worker.start({
+    onUnhandledRequest: (request: Request) => {
+      if (request.url.endsWith('.json')) {
+        console.warn(`Please add a handler for ${request.url}`)
+      }
+    },
+  })
 }
 
 deferRender().then(() => {
